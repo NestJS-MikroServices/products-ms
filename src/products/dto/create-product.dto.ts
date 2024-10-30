362e71b73806a04b9486bbf51662ab693bd364aa
Louis Rey <rlhardrock@gmail.com>
2024-10-30 16:41:00 -0500

    add configuration: code ValidationPipe like useGlobalPipes inside main.ts

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,4 +1,12 @@
+import { IsInt, IsNumber, IsString, Length, Max, Min } from "class-validator";
+
 export class CreateProductDto {
+  @IsString()
+  @Length(10, 20)
   public name: string;
+  @IsNumber()
+  @IsInt()
+  @Min(0)
+  @Max(10)
   public price: string;
 }
